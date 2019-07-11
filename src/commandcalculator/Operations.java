477f648a4c6d7f044a1760811570d3629d310093
Lyceum <48872358+cab-aranw@users.noreply.github.com>
2019-07-11 12:48:52 -0400

    Creating the class Operations
    Creating the class operations and 3 methods. The methods are as follows :
    
    1.- add
    2.- multiply
    3.- division

---
+++ src/commandcalculator/Operations.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commandcalculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author w_ara
+ */
+public class Operations {
+
+    static int val_1;
+    static int val_2;
+    static int result;
+//    static double resultf;
+    static Scanner keyboard = new Scanner(System.in);
+
+    public int add(int val_1, int val_2) 
+    {
+
+        return result;
+    }
+
+    public int multiply(int val_1, int val_2) 
+    {
+
+        return result;
+    }
+
+    public int division(int val_1, int val_2) 
+    {
+
+        return result;
+    }
+
+}
